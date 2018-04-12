df863d6ac0cce3cd151a9bf2db3fb232a76e0ac7
imthe666st <imthe666st@gmail.com>
2018-04-12 21:36:13 +0200

    initial commit

---
+++ Coinflip/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Coinflip
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Code.Coinflip())
+                game.Run();
+        }
+    }
+}
